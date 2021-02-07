9b84f8bcd6d7bc2eacb602f429b7bd9f0a1395b3
Guga Rukhadze <38571128+errkedukke@users.noreply.github.com>
2021-02-07 15:20:44 +0400

    Retriving data from back end into Angular

--- ClientApp/src/app/services/book.service.ts
+++ ClientApp/src/app/services/book.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class BookService {
 
-  constructor() { }
+  _baseUrl: string = "api/Books";
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks()
+  {
+    return this.http.get<Book[]>(this._baseUrl);
+  }
 }
